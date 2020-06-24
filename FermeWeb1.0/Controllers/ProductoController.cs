c3fa54af8f744413b2430d3669ac48be182f7acc
Mauricio Abarca <tercerseis@gmail.com>
2020-06-24 15:45:47 -0400

    Controlador de productos agregado

---
+++ FermeWeb1.0/Controllers/ProductoController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FermeWeb1._0.Models;
+using System.Configuration;
+
+namespace FermeWeb1._0.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //Declarar
+        string tokenProducto = ConfigurationManager.AppSettings["tokenF"].ToString();
+        List<Producto> productoList = new List<Producto>();
+        Producto producto = new Producto();
+
+        //Deserializar
+        public List<Producto> productos()
+        {
+            string url = "/Producto/todos/" + tokenProducto;
+            string resultado = new HerramientasController().calling(url);
+            productoList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Producto>>(resultado);
+            return productoList;
+        }
+
+        //Buscar prod. por nombre
+        public Producto buscarNombre()
+        {
+            string nombre = "Martillo";
+            string url = "/Empleado/buscar/" + nombre + "/" + tokenProducto;
+            string resultado = new HerramientasController().calling(url);
+            producto = Newtonsoft.Json.JsonConvert.DeserializeObject<Producto>(resultado);
+            return producto;
+        }
+    }
+}
