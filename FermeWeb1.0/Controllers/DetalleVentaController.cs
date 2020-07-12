f15feee967606cc40301599ac78ef5a8bd26f736
metal1snake <davidmandiola300@gmail.com>
2020-07-11 20:17:43 -0400

    agregado detalleventa controller y ventas controller

---
+++ FermeWeb1.0/Controllers/DetalleVentaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FermeWeb1._0.Models;
+using System.Configuration;
+
+namespace FermeWeb1._0.Controllers
+{
+    public class DetalleVentaController : Controller
+    {
+        //Declarar
+        string tokenDVenta = ConfigurationManager.AppSettings["tokenF"].ToString();
+        List<DetalleVenta> detVentaList = new List<DetalleVenta>();
+        DetalleVenta detVenta = new DetalleVenta();
+
+        //Deserializar
+        public List<DetalleVenta> detalleVentas()
+        {
+            string url = "/detalleVenta/todos/" + tokenDVenta;
+            string resultado = new HerramientasController().calling(url);
+            detVentaList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<DetalleVenta>>(resultado);
+            return detVentaList;
+        }
+
+
+    }
+}
---
+++ FermeWeb1.0/Controllers/VentasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FermeWeb1._0.Models;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace FermeWeb1._0.Controllers
+{
+    public class VentasController : Controller
+    {
+        string tokenl = ConfigurationManager.AppSettings["tokenF"].ToString();
+
+        public ActionResult crear(int idCli, int idDetalle, int idDoc, int idRetiro, int idEmp, int idFormaPago)
+        {
+            string url = "/Ventas/crear/" + idCli + "/" + idDetalle + "/" + idDoc + "/" + idRetiro + "/" + idEmp + "/" + idFormaPago + "/" + tokenl;
+            string resul = new HerramientasController().calling(url);
+            return View("Home");
+        }
+    }
+}
